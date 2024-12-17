06f801584e9428503864c0c1dab50b6c74715ac6
Elisey Rotar <rotar.elisey.08@itisdalmine.edu.it>
2024-12-17 10:06:13 +0100

    aggiunta di menu'.py

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,12 +1,12 @@
 def Somma():
     n_1=int(input("Inserisci il primo numero: "))
     n_2=int(input("inserisci il secondo numero: "))
-    risultaton_1 + n_2
+    risultato=n_1 + n_2
     print(f"{n_1} + {n_2} = {risultato}")
 def Sottrazione():
     n_1=int(input("Inserisci il primo numero: "))
     n_2=int(input("inserisci il secondo numero: "))
-    risultato n_1n_2
+    risultato= n_1 - n_2
     print(f"{n_1}{n_2} = {risultato}")
 
         
--- menu'.py
+++ menu'.py
@@ -1,7 +1,7 @@
 while True:
     print("Benvenuto nella nostra calcolatrice")
     print("Inserisci l'operazione che vuoi effettuare")
-    scelta int(input("1) sottrazione\n2)addizione\n0)exit\n"))
+    scelta=int(input("1) sottrazione\n2)addizione\n0)exit\n"))
     if scelta==0:
         break
     if scelta == 3:
